194f2fcc9f66c0b5f87f0ca29d501f0d9fbfd120
Dave Townsend <dtownsend@oxymoronical.com>
2007-01-17 22:23:26 +0000

    Config updates

--- bin/startup.bat
+++ bin/startup.bat
@@ -13,10 +13,12 @@ SET UNIX=c:\msys\1.0
 REM This is the base dir of the build environment. Only change if the autodetection fails.
 SET HOME=%~dp0..
 
+SET GECKO_SDK=c:\gecko-sdk
+
 REM --------------------------------------------------------------------------------------
 REM The rest can be left as is.
 
-SET PATH=%UNIX%\bin;%HOME%\tools\moztools\bin;%HOME%\tools\zip;%HOME%\bin;%PATH%
+SET PATH=%UNIX%\bin;%HOME%\tools\moztools\bin;%HOME%\tools\zip;%HOME%\bin;%GECKO_SDK%\bin;%PATH%
 call %VCVARS%
 
 %UNIX%\bin\bash --rcfile %HOME%\bin\buildrc
